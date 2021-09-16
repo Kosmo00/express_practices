74fc10ec097ee7eb79cf28b99556d882b753f117
Kosmo00 <vladiotero19101916@gmail.com>
2021-09-16 16:59:12 -0400

    route /aerolinea created and ready to work on it

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const messages_controller = require('../controllers/messages_controller')
 router.get('/', (req, res) => {
     return res.status(200).render('index')
 })
+router.get('/aerolinea', (req, res) => {
+    return res.status(200).render('aerolinea/index')
+})
 
 router.post('/api/messages', messages_controller.create)
 
