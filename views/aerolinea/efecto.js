8096aeabc7a86b69c46ea955e66b168f19dd8bbf
Daicelys <daicelys@martinez.com>
2021-09-20 21:15:39 -0400

    Creando primera animacion

---
+++ views/aerolinea/efecto.js
@@ -0,0 +1,26 @@
+var writing = str => {
+    let arrFromStr = str.split('');
+    let i = 0;
+    let printStr = setInterval(function () {
+        if (arrFromStr[i] === ' ') {
+            document.body.innerHTML += arrFromStr[i];
+            document.body.innerHTML += arrFromStr[i + 1];
+            i += 2;
+
+
+
+        } else {
+            document.body.innerHTML += arrFromStr[i];
+            i++;
+        }
+        document.body.innerHTML += arrFromStr[i];
+        i++;
+
+        if (i === arrFromStr.length) {
+            clearInterval(printStr);
+            document.body.style.color = "steelblue"
+
+        }
+    }, 5); // 120 es el tiempo que yo le asigné, pueden cambiarlo para que sea más rápido o lento
+};
+writing('Bienvenido, soy una máquina de escribir en JS');
