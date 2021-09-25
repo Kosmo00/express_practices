f42c0ba051999738341d3230cebef52684400923
Daicelys <daicelys@martinez.com>
2021-09-25 17:20:55 -0400

    Formulario1

--- public/js/aerolinea/index.js
+++
@@ -1,9 +0,0 @@
-const typed_strings = ['a nuestra agencia', 'a sus vacaciones', 'a sus viajes', 'a su futuro...']
-
-const typed = new Typed('.text-slider', {
-    strings: typed_strings,
-    typeSpeed: 80,
-    loop: true,
-    backDelay: 1100,
-    backSpeed: 30
-})
