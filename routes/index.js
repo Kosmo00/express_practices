6a893d293f7e6df938ff381a1ba85f26c8eaa831
Kosmo00 <vladiotero19101916@gmail.com>
2021-11-21 12:14:09 -0500

    Adding robespier route

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ const router = express.Router()
 
 const messages_controller = require('../controllers/messages_controller')
 
-router.get('/', (req, res) => {
+router.get('/', (_req, res) => {
     return res.status(200).render('index')
 })
-router.get('/aerolinea', (req, res) => {
+router.get('/aerolinea', (_req, res) => {
     return res.status(200).render('aerolinea/index')
+})
+
+router.get('/robespier', (_req, res) => {
+    return res.status(200).render('robespier/index')
 })
 
 router.post('/api/messages', messages_controller.create)
